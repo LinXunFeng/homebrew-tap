a9e4e57af60b586aa00dd7626fb232cabeebe10b
LinXunFeng <linxunfeng@yeah.net>
2024-02-19 09:01:23 +0800

    version: bump up to 0.0.1

---
+++ condor.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Condor < Formula
+  version "0.0.1"
+  desc "由Dart实现的符号表上传辅助工具"
+  homepage "https://github.com/LinXunFeng/condor"
+  url "https://github.com/LinXunFeng/condor/releases/download/#{version}/condor.tar.gz"
+  sha256 "662dce24a2e0cdb3060d9bf15dd7fd4871377c8196fdfc098deed75836dbcc05"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "condor"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test condor`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
